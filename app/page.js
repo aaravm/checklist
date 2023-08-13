90cd3d22ba8232b528f5fc7f35c9e8493f2ac196
aaravm <32593731+aaravm@users.noreply.github.com>
2023-08-13 17:40:46 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
 import Image from "next/image";
 import Checklist from "../components/Checklist";
 export default function Home() {
   return (
     <main className=" w-full bg-teal-700 min-h-screen  items-center justify-between p-24">
-      <Checklist />
+      
+    <div className='bg-light px-2'>
+      <Checklist /></div>
+    
     </main>
   );
 }
